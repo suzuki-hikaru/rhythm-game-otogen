7924d8df3506680ace6b7b900b41413f3bbaa5d2
hikaru <umisuki20@gmail.com>
2020-12-24 17:35:53 +0900

    feat: playMode追加のため,iframeの追加

--- app/static/JS/playMode.js
+++ app/static/JS/playMode.js
@@ -1,31 +1,34 @@
 /////////　カウント処理/////////
 let count = 0;
+var i = 0;
 let countupVal;
 
 let countup = function () {
     count++;
-    console.log(count);
+    if (count == searchedArray[0][i]) {
+        i = i + 1;
+        console.log("どん！　" + count);
+    } else {
+        console.log(count);
+    };
 };
 
 ///////////　ゲームコントローラー　//////////
-countsToFirebase = [];
+var pushArray = [];
 
 function startGame() {
     player.playVideo();
     count = 0;
+    i = 0;
     countupVal = setInterval('countup();', 100);
+    //// error: 二回目を押すとカウントが早くなる ///////
 };
 
 function stopGame() {
     player.stopVideo();
     clearInterval(countupVal);
-    // window.alert("セーブされた記録が消えます。よろしいですか？");
-    // if (confirm) {
-    //     countsToFirebase.length = 0;
-    // };
 };
 
 function appendCount() {
-    countsToFirebase.push(count);
-    console.log("どん！　" + count);
+    console.log("ポン！　" + count);
 };
