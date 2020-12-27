2bce52e784b416bb9a1b237eee87366da22fdf8c
hikaru <umisuki20@gmail.com>
2020-12-27 23:21:44 +0900

    feat:rhythm 表示のため,インスタス作成テスト追加

--- app/python/gspread_practice.py
+++ app/python/gspread_practice.py
@@ -1,19 +1,38 @@
 import gspread
+import json
+
+#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
 from oauth2client.service_account import ServiceAccountCredentials 
- 
+
+#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
- 
-# 秘密鍵（JSONファイル）のファイル名を入力
-credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet-sample.json', scope)
+
+#認証情報設定
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+credentials = ServiceAccountCredentials.from_json_keyfile_name('config.json', scope)
+
+#OAuth2の資格情報を使用してGoogle APIにログインします。
 gc = gspread.authorize(credentials)
- 
-# ↑ここまでは毎回同じ内容
-# ↓ここからは毎回違う内容（ここから記述） 
- 
- 
-# 「キー」でワークブックを取得
-SPREADSHEET_KEY = '対象のスプレッドシートキー'
-wb = gc.open_by_key(SPREADSHEET_KEY)
- 
-# 「sheet1」で一番左のシートを取得
-ws = wb.sheet1
+
+#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
+# SPREADSHEET_KEY = '1LecCnNZvPWf6w3cMp2HD9EZxslSTSFpwxjtcILBZJ-M' ##テスト用
+SPREADSHEET_KEY = '1YmMxOOkyu7hrPy9o6TIF2S-MBjxcmZzm-UT8cZK25os' ##compose
+
+#共有設定したスプレッドシートのシート1を開く
+worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+
+
+"""
+ここからデータの取得と更新
+"""
+
+col_list_time = worksheet.col_values(3)
+
+#A1セルの値を受け取る
+# import_value = int(worksheet.acell('A1').value)
+
+print(col_list_time)
+
+#A1セルの値に100加算した値をB1セルに表示させる
+# export_value = import_value+100
+# worksheet.update_cell(1,2, export_value)
--- app/static/JS/playMode.js
+++ app/static/JS/playMode.js
@@ -38,3 +38,90 @@ function appendCount() {
         console.log("ポン！　○　" + count);
     };
 };
+
+//////////　ゲームui　//////////
+let GameObject = function () { // GameObject のコンストラクタ関数
+    this.image = new Image();
+    this.image.style.position = "fixed";
+}
+
+let PlayBar = function () { // PlayBar のコンストラクタ関数
+    GameObject.apply(this, arguments);
+    this.x = 0;
+    this.y = 0;
+    this.image.src = "/static/images/taiko.png";
+}
+
+let CountBall = function () { // CountBall のコンストラクタ関数
+    GameObject.apply(this, arguments);
+    this.x = Math.floor(Math.random() * window.innerWidth - this.image.width);
+    this.y = Math.floor(Math.random() * window.innerHeight - this.image.height);
+    this.dx = 0;
+    this.dy = 1;
+    this.image.src = "/static/images/taiko.png";
+}
+
+PlayBar.prototype = new GameObject;
+CountBall.prototype = new GameObject;
+
+CountBall.prototype.update = function () {
+    this.x = this.x + this.dx;
+    this.y = this.y + this.dy;
+    if (this.x < 0 || this.x > window.innerWidth - this.image.width) {
+        this.x = this.x - this.dx;
+        this.dx *= -1;
+    }
+    if (this.y < 0 || this.y > window.innerHeight - this.image.height) {
+        this.y = this.y - this.dy;
+        this.dy *= -1;
+    }
+    this.image.style.left = this.x + "px";
+    this.image.style.top = this.y + "px";
+    if ((Math.abs(PlayBar.x - this.x) < PlayBar.image.width) && (Math.abs(PlayBar.y - this.y) < PlayBar.image.height)) {
+        // gameover();
+    }
+}
+
+let playBar;
+let countball = new Array(5);
+
+function gameView() {　// ブラウザにコンテンツがロードされたときに1度だけ呼ばれる関数
+    playBar = new PlayBar();
+
+    for (let i = 0; i < countball.length; i++) {
+        countball[i] = new CountBall();
+    }
+    document.body.appendChild(PlayBar.image);
+
+    for (let i = 0; i < countball.length; i++) {
+        document.body.appendChild(countball[i].image);
+    }
+
+    // マウスが移動したときに MouseMove 関数が呼ばれるようにイベントリスナを設定
+    if (document.addEventListener) {
+        document.addEventListener("mousemove", MouseMove);
+    } else if (document.attachEvent) {
+        document.attachEvent("onmousemove", MouseMove);
+    }
+}
+
+function MouseMove(e) {
+    PlayBar.x = (e.clientX - PlayBar.image.width / 2);
+    PlayBar.y = (e.clientY - PlayBar.image.height / 2);
+    PlayBar.image.style.left = PlayBar.x + "px";
+    PlayBar.image.style.top = PlayBar.y + "px";
+}
+
+let update = function () { // 画像表示のアップデート
+    // for (let i = 0; i < countball.length; i++) {
+    //     countball[i].update();
+    // }
+}
+
+// updateVal = setInterval(update, 10); // 関数updateを10msごとに呼び出す．
+
+// function gameover() {
+//     clearInterval(countupVal); // 関数countupを一定時間ごとに呼び出していたのを止める．
+//     clearInterval(updateVal); // 関数updateを一定時間ごとに呼び出していたのを止める．
+// }
+
