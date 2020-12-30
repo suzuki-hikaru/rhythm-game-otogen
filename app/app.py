b85b2711fe8072778dbbc6d959b54e735a1fd84f
hikaru <umisuki20@gmail.com>
2020-12-30 23:45:21 +0900

    feat: セレクト機能充実のため,docKey挿入＋esp32登録機能の追加

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from pytube import YouTube
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials 
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -37,9 +40,6 @@ def post_create():
     video.download('./app/static/movies/')
     return render_template("createMode.html", url = url_name, yt_title = video_name, path = './app/static/movies/' + video_name + '.mp4')
 
-"""""""""""""""""""""""""""
-###コーディング中###
-"""""""""""""""""""""""""""
 @app.route("/playMode", methods=['GET'])
 def get_play():
     error = "youtubeURLを入力して下さい"
@@ -55,6 +55,23 @@ def post_play():
 def post_doc():
     doc_name = request.form['name']
     return render_template("docPlayMode.html", doc_name = doc_name,)
+
+@app.route("/createData", methods=['GET','POST'])
+def esp32():
+    ss_url = request.form['name']
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('app/static/key/config.json', scope)
+    gc = gspread.authorize(credentials)
+    # SPREADSHEET_KEY = '1LecCnNZvPWf6w3cMp2HD9EZxslSTSFpwxjtcILBZJ-M' ##テスト用
+    # SPREADSHEET_KEY = '1YmMxOOkyu7hrPy9o6TIF2S-MBjxcmZzm-UT8cZK25os' ##compose
+    SPREADSHEET_KEY = ss_url
+    worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+    col_list_time = worksheet.col_values(3)
+    return render_template("createData.html",countTimes=col_list_time)
+
+@app.route("/createEsp", methods=['POST'])
+def createEsp():
+    return render_template("selectMode.html",)
 
 @app.route("/test", methods=['GET','POST'])
 def test():
--- app/static/JS/selectMode.js
+++ app/static/JS/selectMode.js
@@ -1,26 +1,81 @@
 function getData() {
-    // firebaseの設定
-    var userId = firebase.auth().currentUser.uid;
-    //取得するデータ範囲の設定
-    firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
-        var data = snapshot.val();
-        for (key in data) {
-            var oneCountTime = data[key];
-            console.log("firebase:" + oneCountTime.countTimes);
-            document.getElementById("test").innerHTML += '<div>' + oneCountTime.title + '：' + oneCountTime.countTimes + '</div>';
-        }
+    // inform: テーブルの削除　＊粗雑
+    var Table = document.getElementById("table");
+    Table.innerHTML = "";
 
-        ////////　メモ書き　///////////////////////////////////////////////////////////////
-        //データのうちtimesを取得する.'Anonymous'は入力されていないデータ又は匿名データ。
-        // var data = (snapshot.val() && snapshot.val().times) || 'Anonymous'; 
-        // 指定したリファレンスの全体
-        // var data = snapshot.val();
-        //ユーザID
-        // var data = snapshot.key;
-        //keyの名前
-        // var data = snapshot.child('countTimes').key;
-        // setだけの時
-        // var data = snapshot.child('countTimes').val();
-        //////////////////////////////////////////////////////////////////////////////////
+    //取得するデータ範囲の設定
+    firebase.database().ref('/users/').once('value').then(function (snapshot) {
+        var allData = snapshot.val();
+        const th = '<tr id="th">\n<th>Youtubeタイトル</th><th>ファイル名</th><th>作成者</th><th>いいね数</th><th>jump</th>\n</tr>';
+        document.getElementById('table').insertAdjacentHTML('afterbegin', th);
+        //繰り返してタイトルを取得。多分推奨のやり方ではない
+        for (oneUserId in allData) {
+            var oneUser = allData[oneUserId];
+            for (oneDocKey in oneUser) {
+                const tr = `<tr>\n<form action="/playMode method="POST" enctype="multipart/form-data"><td>${allData[oneUserId][oneDocKey].yt_title}</td><td>${allData[oneUserId][oneDocKey].title}</td><td>${allData[oneUserId][oneDocKey].userName}</td><td>${allData[oneUserId][oneDocKey].good}</td><td><button onclick="jump('${oneDocKey}');">jump</button></td></form>\n</tr>`;
+                document.getElementById('th').insertAdjacentHTML('afterend', tr);
+            };
+        };
     });
 };
+
+function getTitleData() {
+    // inform: テーブルの削除　＊粗雑
+    const Table = document.getElementById("table");
+    Table.innerHTML = "";
+    // formからの取得データ
+    const titledata = document.getElementById("searchTitle").value;
+    //取得するデータ範囲の設定
+    firebase.database().ref('/users/').once('value').then(function (snapshot) {
+        var allData = snapshot.val();
+        const th = '<tr id="th">\n<th>Youtubeタイトル</th><th>ファイル名</th><th>作成者</th><th>いいね数</th><th>jump</th>\n</tr>';
+        document.getElementById('table').insertAdjacentHTML('afterbegin', th);
+
+        //繰り返してタイトルを取得。多分推奨のやり方ではない
+        for (oneUserId in allData) {
+            var oneUser = allData[oneUserId];
+            for (oneDocKey in oneUser) {
+                if (allData[oneUserId][oneDocKey].title.indexOf(titledata) > -1 || allData[oneUserId][oneDocKey].yt_title.indexOf(titledata) > -1) {
+                    // 部分一致のときの処理
+                    const tr = `<tr>\n<form action="/playMode method="POST" enctype="multipart/form-data"><td>${allData[oneUserId][oneDocKey].yt_title}</td><td>${allData[oneUserId][oneDocKey].title}</td><td>${allData[oneUserId][oneDocKey].userName}</td><td>${allData[oneUserId][oneDocKey].good}</td><td><button onclick="jump('${oneDocKey}');">jump</button></td></form>\n</tr>`;
+                    document.getElementById('th').insertAdjacentHTML('afterend', tr);
+                }
+            };
+        };
+    });
+};
+
+function getURLData() {
+    // inform: テーブルの削除　＊粗雑
+    const Table = document.getElementById("table");
+    Table.innerHTML = "";
+
+    // formからの取得データ
+    const urldata = document.getElementById("searchURL").value;
+    //取得するデータ範囲の設定
+    firebase.database().ref('/users/').once('value').then(function (snapshot) {
+        var allData = snapshot.val();
+        const th = '<tr id="th">\n<th>Youtubeタイトル</th><th>ファイル名</th><th>作成者</th><th>いいね数</th><th>jump</th>\n</tr>';
+        document.getElementById('table').insertAdjacentHTML('afterbegin', th);
+
+        //繰り返してタイトルを取得。多分推奨のやり方ではない
+        for (oneUserId in allData) {
+            var oneUser = allData[oneUserId];
+            for (oneDocKey in oneUser) {
+                if (urldata == allData[oneUserId][oneDocKey].url) {
+                    const tr = `<tr>\n<form action="/playMode method="POST" enctype="multipart/form-data"><td>${allData[oneUserId][oneDocKey].yt_title}</td><td>${allData[oneUserId][oneDocKey].title}</td><td>${allData[oneUserId][oneDocKey].userName}</td><td>${allData[oneUserId][oneDocKey].good}</td><td><button onclick="jump('${oneDocKey}');">jump</button></td></form>\n</tr>`;
+                    document.getElementById('th').insertAdjacentHTML('afterend', tr);
+                }
+            };
+        };
+    });
+};
+
+function jump(oneDocKey) {
+    // var jumpId = document.getElementById(oneDocKey).value;
+    // console.log(jumpId);
+    console.log(oneDocKey);
+    document.getElementById("play").value = oneDocKey;
+    document.getElementById("play").style.backgroundColor = "yellow";
+    document.getElementById("info").innerHTML = "<p>＊docKeyが挿入されました。</p>";
+}
